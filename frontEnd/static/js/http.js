4d2a031b8e4448d9f8db24bdd4fb57ffbae3c30f
junlee1998 <48245709+junlee1998@users.noreply.github.com>
2019-10-15 13:41:22 +0800

    Update http.js

--- frontEnd/static/js/http.js
+++ frontEnd/static/js/http.js
@@ -1,15 +1,8 @@
-/**
- * Created by superman on 17/2/16.
- * http配置
- */
-
 import axios from 'axios'
-// import store from './store/store'
-// import * as types from './store/types'
+
 
 // axios 配置
 axios.defaults.timeout = 5000
-// axios.defaults.baseURL = 'https://api.github.com'
 
 // http request 拦截器
 axios.interceptors.request.use(
