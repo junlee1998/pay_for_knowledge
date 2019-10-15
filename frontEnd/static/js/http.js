2424e60558135b4b19ec91b2fd2c27578e9555cb
junlee1998 <48245709+junlee1998@users.noreply.github.com>
2019-10-15 13:40:54 +0800

    Update http.js

--- frontEnd/static/js/http.js
+++ frontEnd/static/js/http.js
@@ -21,29 +21,4 @@ axios.interceptors.request.use(
   },
 );
 
-// // http response 拦截器
-// axios.interceptors.response.use(
-//   response => {
-//     return response
-//   },
-//   error => {
-//     if (error.response) {
-//       switch (error.response.status) {
-//         case 401:
-//           // 401 清除token信息并跳转到登录页面
-//           store.commit(types.LOGOUT)
-//
-//           // 只有在当前路由不是登录页面才跳转
-//           router.currentRoute.path !== 'login' &&
-//           router.replace({
-//             path: 'login',
-//             query: { redirect: router.currentRoute.path },
-//           })
-//       }
-//     }
-//     // console.log(JSON.stringify(error));//console : Error: Request failed with status code 402
-//     return Promise.reject(error.response.data)
-//   },
-// )
-
 export default axios
