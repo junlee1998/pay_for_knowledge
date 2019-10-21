b84e09491d92dc755dac49231c2e78fa432a195f
junlee1998 <malijun9077@qq.com>
2019-10-21 14:11:06 +0800

    change

--- frontEnd/static/js/http.js
+++ frontEnd/static/js/http.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-
+import store from '../../src/store/store'
 
 // axios 配置
-axios.defaults.timeout = 5000
+axios.defaults.timeout = 5000;
+
 
 // http request 拦截器
 axios.interceptors.request.use(
@@ -14,6 +15,21 @@ axios.interceptors.request.use(
   },
 );
 
-
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          // 401 清除信息并跳转到登录页面
+          alert("账号或者密码错误");
+      }
+    }
+    return Promise.reject(error.response.data)
+  },
+);
 
 export default axios
